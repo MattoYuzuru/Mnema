7cc29082bed058e75de52d71cc3354ddffd25fcd
Matvei Riabushkin <matveybobr937@gmail.com>
2025-10-27 15:34:08 +0300

    feat: Add auth service from start.spring.io

---
+++ backend/services/auth/src/test/kotlin/app/mnema/auth/AuthApplicationTests.kt
@@ -0,0 +1,13 @@
+package app.mnema.auth
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
