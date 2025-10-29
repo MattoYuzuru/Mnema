a033871bf9df5de2a19ea191f85b71854bce802e
Matvei Riabushkin <matveybobr937@gmail.com>
2025-10-29 13:05:55 +0300

    feat: New deploy config, .env all sercrets, generate google oauth 2.0, twst containers via local docker build

--- backend/services/auth/build.gradle.kts
+++ backend/services/auth/build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.flywaydb:flyway-core")
+    implementation("org.flywaydb:flyway-database-postgresql")
     runtimeOnly("org.postgresql:postgresql")
 
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
--- backend/services/user/build.gradle.kts
+++ backend/services/user/build.gradle.kts
@@ -10,6 +10,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.flywaydb:flyway-core")
+    implementation("org.flywaydb:flyway-database-postgresql")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
     runtimeOnly("org.postgresql:postgresql")
