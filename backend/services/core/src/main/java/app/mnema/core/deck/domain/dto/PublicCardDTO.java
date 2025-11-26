9c1cf9d57e2da494130d5b075089d631cf46bdd9
Matvei Riabushkin <matveybobr937@gmail.com>
2025-11-26 13:29:04 +0300

    feat(core): Add new fields to PCdto, UDdto

--- backend/services/core/src/main/java/app/mnema/core/deck/domain/dto/PublicCardDTO.java
+++ backend/services/core/src/main/java/app/mnema/core/deck/domain/dto/PublicCardDTO.java
@@ -1,13 +1,21 @@
 package app.mnema.core.deck.domain.dto;
 
+import app.mnema.core.deck.domain.entity.PublicDeckEntity;
 import com.fasterxml.jackson.databind.JsonNode;
 
+import java.time.Instant;
 import java.util.UUID;
 
 public record PublicCardDTO(
+        Integer deckVersion,
         UUID cardId,
+        PublicDeckEntity deck,
+        JsonNode content,
         int orderIndex,
         String[] tags,
-        JsonNode content
+        Instant createdAt,
+        Instant updatedAt,
+        boolean active,
+        String checksum
 ) {
 }
--- backend/services/core/src/main/java/app/mnema/core/deck/domain/dto/UserDeckDTO.java
+++ backend/services/core/src/main/java/app/mnema/core/deck/domain/dto/UserDeckDTO.java
@@ -1,18 +1,23 @@
 package app.mnema.core.deck.domain.dto;
 
+import com.fasterxml.jackson.databind.JsonNode;
+
 import java.time.Instant;
 import java.util.UUID;
 
 public record UserDeckDTO(
         UUID userDeckId,
+        UUID userId,
         UUID publicDeckId,
         Integer subscribedVersion,
         Integer currentVersion,
+        boolean autoUpdate,
+        String algorithmId,
+        JsonNode algorithmParams,
         String displayName,
         String displayDescription,
-        String algorithmId,
-        boolean autoUpdate,
-        boolean archived,
-        Instant lastSyncedAt
+        Instant createdAt,
+        Instant lastSyncedAt,
+        boolean archived
 ) {
 }
