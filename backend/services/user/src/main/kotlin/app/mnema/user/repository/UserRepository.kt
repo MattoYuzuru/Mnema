e6f0dabe5e829ad416b380f074b8c9bfe86c51b6
Matvei Riabushkin <matveybobr937@gmail.com>
2025-12-22 13:25:01 +0300

    fix(user): Fix user authentication issues

--- backend/services/user/src/main/kotlin/app/mnema/user/repository/UserRepository.kt
+++ backend/services/user/src/main/kotlin/app/mnema/user/repository/UserRepository.kt
@@ -2,6 +2,8 @@ package app.mnema.user.repository
 
 import app.mnema.user.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
 import java.util.*
 
 interface UserRepository : JpaRepository<User, UUID> {
@@ -11,4 +13,8 @@ interface UserRepository : JpaRepository<User, UUID> {
 
     fun existsByUsernameIgnoreCaseAndIdNot(username: String, id: UUID): Boolean
     fun existsByEmailIgnoreCaseAndIdNot(email: String, id: UUID): Boolean
+
+    @Modifying
+    @Query(value = "UPDATE app_user.users SET id = :newId WHERE id = :oldId", nativeQuery = true)
+    fun migrateId(oldId: UUID, newId: UUID): Int
 }
