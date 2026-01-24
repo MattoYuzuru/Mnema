beb44bbf19210fccbfa32a6bf677ec95f9bf9c9c
Matvei Riabushkin <matveybobr937@gmail.com>
2026-01-24 21:26:47 +0300

    feat(ai): Add basic SecurityConfig, split propeties for dev/prod/test layers

--- backend/services/ai/src/main/java/app/mnema/ai/AiApplication.java
+++ backend/services/ai/src/main/java/app/mnema/ai/AiApplication.java
@@ -2,7 +2,9 @@ package app.mnema.ai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class AiApplication {
 
